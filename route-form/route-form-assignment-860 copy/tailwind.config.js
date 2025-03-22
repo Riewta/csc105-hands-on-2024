21cd56bd22f5b411b89fb85fffef271fb4237f10
Riewta <riewtaa@gmail.com>
2025-03-23 02:30:00 +0700

    Add files via upload

---
+++ route-form/route-form-assignment-860 copy/tailwind.config.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-undef */
+/*if i don't do the first line i can't export the module*/
+module.exports = {
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
