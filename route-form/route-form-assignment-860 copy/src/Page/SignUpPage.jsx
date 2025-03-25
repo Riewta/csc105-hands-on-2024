5fe3573b7b3c126f929072ecc341ed875c640aa3
Riewta <riewtaa@gmail.com>
2025-03-25 12:41:11 +0700

    Delete route-form/route-form-assignment-860 copy directory

--- route-form/route-form-assignment-860 copy/src/Page/SignUpPage.jsx
+++
@@ -1,9 +0,0 @@
-function SignUpPage() {
-    return (
-        <div className="flex flex-col items-center justify-center h-screen">
-            <h1 className="text-3xl font-bold">SignUpPage</h1>
-        </div>
-    );
-}
-
-export default SignUpPage;
--- route-form/route-form-assignment-860 copy/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable no-undef */
-/*if i don't do the first line i can't export the module*/
-module.exports = {
-    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-    theme: {
-        extend: {},
-    },
-    plugins: [],
-}
